e2d14d2bd7accc40a641ee602df9118bbad9e313
Akshay Raul <akshay.raul@stud.hochschule-heidelberg.de>
2021-05-08 18:54:02 +0200

    negligible change

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const hostname = '127.0.0.3';
 const port = 3000;
 
-
 var request = require('request');
 request('http://www.omdbapi.com/?apikey=9030182c&s=space&y=2001', function (error, response, body) {
   if (!error && response.statusCode == 200) {
