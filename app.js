3d4bf19ba2b38a7c7c2b6fd07d7a9c621e7e38c7
Akshay-Raul <akshay.raul@stud.hochschule-heidelberg.de>
2021-05-12 14:26:55 +0200

    Fetching API data

---
+++ app.js
@@ -0,0 +1,48 @@
+const { Client } = require('@elastic/elasticsearch')
+const config = require('config');
+const request = require('request');
+const elasticConfig = config.get('elastic');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+const client = new Client({
+    cloud: {
+        id: elasticConfig.cloudID
+    },
+    auth: {
+        username: elasticConfig.username,
+        password: elasticConfig.password
+    }
+})
+
+app.get('/', (req, res) => {
+    res.send('app up and running ')
+})
+
+app.get('/refresh', (req, res) => {
+    fetchMovieData()
+    res.send('refresh completed')
+})
+
+function fetchMovieData() {
+    request('http://www.omdbapi.com/?apikey=9030182c&s=space&y=2001', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            let movieData = JSON.parse(body);
+            movieData.Search.forEach(function (element, index) {
+                request('http://www.omdbapi.com/?apikey=9030182c&i=' + element.imdbID, function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        console.log(JSON.parse(body));
+                    }
+                })
+            });
+        }
+    })
+}
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at ${port}`)
+})
+
