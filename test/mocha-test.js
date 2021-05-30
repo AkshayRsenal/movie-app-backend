16cb4dfadd2a608107ede140f057ad772a7767f0
Akshay-Raul <akshay.raul@stud.hochschule-heidelberg.de>
2021-05-30 22:08:04 +0200

    datatype changed

--- test/mocha-test.js
+++ test/mocha-test.js
@@ -1,10 +1,10 @@
 const request = require('request');
-var chai = require('chai');
+let chai = require('chai');
 let chaiHttp = require('chai-http');
 let mainjs = require('../app.js')
-var expect = require('chai').expect;
-var arrayJsonObjElements = ["Title","Year","imdbID","Poster"];
-var imdbIDSpecificElements = ["Title","Director","Plot","Poster"]
+let expect = require('chai').expect;
+let arrayJsonObjElements = ["Title","Year","imdbID","Poster"];
+let imdbIDSpecificElements = ["Title","Director","Plot","Poster"]
 
 chai.use(chaiHttp);
 
@@ -24,7 +24,7 @@ describe("[Test movie-app-backend] /refresh [Routing]", function(){
 
     it('Valid JSON Elements Response in OMDB-API Request without imdbID', (done)=>{
         request('http://www.omdbapi.com/?apikey=9030182c&s=space&y=2001', function (error, response, body) {
-            var movieData = JSON.parse(body);
+            let movieData = JSON.parse(body);
             // console.log(movieData.Search[0])
             if(arrayJsonObjElements.every(property=>{ return movieData.Search[0].hasOwnProperty(property) }) ){
                 done();
@@ -34,7 +34,7 @@ describe("[Test movie-app-backend] /refresh [Routing]", function(){
 
     it('Valid Response from OMDB-API with hardcoded imdbID', (done)=>{
         request('http://www.omdbapi.com/?apikey=9030182c&i=tt0275848', function (error, response, body) {
-            var idSpecificMovieData = JSON.parse(body);
+            let idSpecificMovieData = JSON.parse(body);
             // console.log(movieData)
             if(imdbIDSpecificElements.every(property=>{ return idSpecificMovieData.hasOwnProperty(property) }) ){
                 done();
